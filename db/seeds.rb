02d327f2c4839418d3195fbb50f9e92e50638297
Hannah Osei <hannahosei97@gmail.com>
2025-01-16 13:20:56 +0000

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Owner.destroy_all
+Car.destroy_all
+
+owner1 = Owner.create!(nickname: "Bruce")
+owner2 = Owner.create!(nickname: "Amy")
+owner3 = Owner.create!(nickname: "Jane")
+
+# Create Cars
+Car.create!(brand: "Jaguar", model: "I-Pace", year: 2024, fuel: "Electric", owner: owner1)
+Car.create!(brand: "Mercedes", model: "C-class", year: 2020, fuel: "Unleaded petrol", owner: owner2)
+Car.create!(brand: "Tesla", model: "Model X", year: 2023, fuel: "Electric", owner: owner3)
